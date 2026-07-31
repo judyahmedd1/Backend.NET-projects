0504f1e94cda20b42498c16375737fda2ade4af0
judyahmedd1 <judyahmed155@gmail.com>
2026-07-31 06:59:22 +0300

    Add files via upload

---
+++ BankSystemProject.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    internal class BankSystem
+    {
+        //lists for creating an account 
+        static List<int> accnumbers = new List<int>();
+        static List<string> accnames = new List<string>();
+        static List<double> accbalance = new List<double>();
+        static List<string> accpins = new List<string>();
+        static List<string> histories = new List<string>();
+
+        static Random random = new Random();
+        static int currentuser = -1;
+        static void CreateAccount()
+        {
+            int accountnumber = random.Next(1, 1000);
+            Console.WriteLine("your account number is: " + accountnumber);
+
+            Console.WriteLine("enter account name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter pin");
+            string pin = Console.ReadLine();
+            Console.WriteLine("enter balance");
+            double balance = double.Parse(Console.ReadLine());
+
+            accnumbers.Add(accountnumber);
+            accnames.Add(name);
+            accpins.Add(pin);
+            accbalance.Add(balance);
+            histories.Add("");
+        }
+        static void Login()
+        {
+            Console.WriteLine("enter account number ");
+            int number = int.Parse((Console.ReadLine()));
+
+            Console.WriteLine("enter pin ");
+            string pin = Console.ReadLine();
+
+            for (int i = 0; i < accnumbers.Count; i++)
+            {
+                if (accnumbers[i] == number && accpins[i] == pin)
+                {
+                    currentuser = i;
+                    Console.WriteLine("login done");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Wrong account number or pin.");
+        }
+        static void Deposit()
+        {
+            Console.WriteLine("enter amount to deposit ");
+            double amount = double.Parse(Console.ReadLine());
+            accbalance[currentuser] += amount;
+            histories[currentuser] += $"deposit: {amount} , balance now : {accbalance[currentuser]}\n"; ;
+        }
+
+        static void Withdraw()
+        {
+            Console.WriteLine("enter amount to withdraw ");
+            double withdrawamount = double.Parse(Console.ReadLine());
+            if (accbalance[currentuser] < withdrawamount)
+            {
+                Console.WriteLine("current balance is not enough");
+            }
+            else
+            {
+                accbalance[currentuser] -= withdrawamount;
+                histories[currentuser] += $"withdraw: {withdrawamount} , balance now : {accbalance[currentuser]}\n"; ;
+            }
+        }
+            static void CheckBalance()
+        {
+            Console.WriteLine("current balance is: " + accbalance[currentuser]);
+        }
+
+        static void ViewHistory()
+        {
+            int accnum = accnumbers[currentuser];
+            Console.WriteLine($"history of {accnum} is {histories[currentuser]}\n") ;
+        }
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                if (currentuser == -1)
+                {
+                    Console.WriteLine("Menu is 1. create account\n 2.login\n 3.exit");
+                    int choice = int.Parse((Console.ReadLine()));
+
+                    switch (choice)
+                    {
+                        case 1:
+                            CreateAccount();
+                            break;
+
+                        case 2:
+                            Login();
+                            break;
+
+                        case 3:
+                            exit = true;
+                            break;
+                    }
+                }
+                else
+                { 
+                    Console.WriteLine("1.check balance");
+                    Console.WriteLine("2.deposit");
+                    Console.WriteLine("3.withdraw");
+                    Console.WriteLine("4.transaction history");
+                    Console.WriteLine("5.logout");
+                    Console.WriteLine("enter your choice: ");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("balance is: " + accbalance[currentuser]);
+                            break;
+
+                        case 2:
+                            Deposit();
+                            break;
+
+                        case 3:
+                            Withdraw();
+                            break;
+
+                        case 4:
+                            ViewHistory();
+                            break;
+
+                        case 5:
+                            currentuser = -1;
+                            Console.WriteLine("logged out.");
+                            break;
+                    }
+                }
+            }
+        
+
+    }
+       
+    }
+}
---
+++ project1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projects_1
+{
+    internal class Program
+    {
+        static double GetAverage(List<double> grades)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < grades.Count; i++)
+            {
+                sum += grades[i];
+            }
+            return sum / grades.Count;
+        }
+        static void Main(string[] args)
+        {
+            //Store student names in a list of grades.
+            //            Calculate the average grade for each student.
+            //Assign a random grade level to each student
+            //            Freshman, Sophomore, Junior, Senior.
+            //            Generate and display a formatted report of all studen
+            //            Project - Student Grades Tracker
+            //            Rule for Level Assignment
+            //            Average 0.0 – 20 → Freshman
+            //            Average 20 – 40 → Sophomore
+            //            Average 40 – 60 → Junior
+            //            Average 60 – 100 → Senior
+
+
+            List<string> students = new List<string>();
+            students.Add("judy");
+            students.Add("hana");
+            students.Add("menna");
+
+            List<double> judygrades = new List<double> { 15, 18, 20 };
+            List<double> hanagrades = new List<double> { 35, 40, 38 };
+            List<double> mennagrades = new List<double> { 55, 60, 58 };
+            List<List<double>> allGrades = new List<List<double>> {
+               judygrades, hanagrades, mennagrades
+            };
+
+            string randomlevel;
+            for (int i = 0; i < students.Count; i++)
+            {
+                double average = GetAverage(allGrades[i]);
+                if (average >= 0.0 && average <= 20.0)
+                {
+                    randomlevel = "Freshman";
+                }
+                else if (average > 20.0 && average <= 40.0)
+                {
+                    randomlevel = "Sophomore";
+                }
+                else if (average > 40.0 && average <= 60.0)
+                {
+                    randomlevel = "Junior";
+                }
+                else
+                    randomlevel = "Senior";
+            }
+
+        }
+    }
+}
